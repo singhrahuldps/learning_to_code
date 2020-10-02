a52441ca7750f740e0f58857a66616b88f6b42e6
Rahul <singh.rahuldps@gmail.com>
2020-10-02 22:46:59 +0530

    Create forloop.kt

---
+++ forloop.kt
@@ -0,0 +1,9 @@
+fun main() {
+  // creates an immutable list
+  val names = listOf("Rahul", "Rohit", "Varun")
+  
+  // prints each name in new line
+  for (name in names) {
+      println(name)
+  }
+}
