6b2eddf6a1e67ce49e25ce895424a1bd124c1662
rohanrko <rohanrko96.6@gmail.com>
2020-10-02 23:23:07 +0530

    Add files via upload

---
+++ addedJava.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class hacktober {
+	public static void main (String[] args) {
+		
+		String arr[] = new String[] {"Rahul", "Rohit", "Varun"};
+		for (int i=0; i<arr.length; i++) {
+		    System.out.println(arr[i]);
+		}
+		
+	}
+}
